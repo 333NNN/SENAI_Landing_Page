5d516bcf3ec958386b59577b2cc1492533280350
333NNN <nata.cunha@aluno.senai.br>
2025-05-18 23:34:50 -0300

    últimas modificações estéticas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,4 @@
-import Container from "react-bootstrap/Container";
-import Col from "react-bootstrap/Col";
-import Card from "react-bootstrap/Row";
+
 import Cursos from "./Cursos";
 import Sobre from "./Sobre";
 import Modalidades from "../components/Modalidades";
